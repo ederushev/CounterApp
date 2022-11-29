ae5cbc82490cfcce7e522bdf233740897041b369
ederushev <118970733+ederushev@users.noreply.github.com>
2022-11-29 20:17:23 +0300

    Исправил замечания

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var counter: Int = 0
+    private var counter: Int = 0 {
+        didSet {
+            counterLabel.text = "Значение счётчика: \(counter)"
+        }
+    }
     @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +22,7 @@ class ViewController: UIViewController {
 
     @IBAction func counterButton(_ sender: Any) {
     counter += 1
-        counterLabel.text = "Значение счётчика: \(counter)"
+       // counterLabel.text = "Значение счётчика: \(counter)"
     }
 }
 
